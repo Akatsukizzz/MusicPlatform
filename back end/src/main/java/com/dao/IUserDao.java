276e549ae8aecbd6dfde9ccc84c746921fa25045
Akatsuki <akatsukizzz@icloud.com>
2020-08-28 14:07:07 +0800

    分离了用户模块和关注/动态模块以及音乐/歌单模块

--- back end/src/main/java/com/dao/IUserDao.java
+++ back end/src/main/java/com/dao/IUserDao.java
@@ -1,11 +1,12 @@
 package com.dao;
 
-import com.domain.*;
-import org.apache.ibatis.annotations.*;
-import org.apache.ibatis.mapping.FetchType;
+import com.domain.User;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -59,122 +60,6 @@ public interface IUserDao {
      */
     @Select("select uid, phone, username, sex, birthday, location from User where phone = #{phone}")
     User findUserByPhone (String phone);
-    
-    /**
-     * 查找用户所有的关注
-     *
-     * @param uid 用户 uid
-     * @return 用户关注列表
-     */
-    @Select("select * from #{uid}_Following")
-    List<FollowUser> findAllFollowing (Integer uid);
-    
-    /**
-     * 查找用户关注的 uid
-     *
-     * @param uid 用户 uid
-     * @return 用户关注列表的 uid 列表
-     */
-    @Select("select uid from #{uid}_Following")
-    List<Integer> findAllFollowingUid (Integer uid);
-    
-    /**
-     * 查找用户所有的粉丝
-     *
-     * @param uid 用户 uid
-     * @return 用户粉丝列表
-     */
-    @Select("select * from #{uid}_Followers")
-    List<FollowUser> findAllFollower (Integer uid);
-    
-    /**
-     * 查找用户粉丝的 uid
-     *
-     * @param uid 用户 uid
-     * @return 用户粉丝的 uid 列表
-     */
-    @Select("select uid from #{uid}_Followers")
-    List<Integer> findAllFollowersUid (Integer uid);
-    
-    /**
-     * 查找用户的全部歌单
-     *
-     * @param uid 用户 uid
-     * @return 用户的歌单列表
-     */
-    @Results(id = "songMap", value = {
-            @Result(column = "fullName", property = "songs", many = @Many(select = "com.dao.IUserDao.findAllSongInList", fetchType = FetchType.EAGER))
-    })
-    @Select("select * from SongList where uid = #{uid}")
-    List<SongList> findAllSongList (Integer uid);
-    
-    /**
-     * 查找用户指定的歌单
-     *
-     * @param uid       用户 uid
-     * @param listIndex 歌单索引
-     * @return 用户的歌单，若不存在则返回 null
-     */
-    @Select("select * from SongList where uid = #{uid} and listIndex = #{listIndex}")
-    SongList findSongList (@Param("uid") Integer uid, @Param("listIndex") Integer listIndex);
-    
-    /**
-     * 查找用户歌单在数据库中对应的表名
-     *
-     * @param uid       用户 uid
-     * @param listIndex 用户歌单索引
-     * @return 歌单表全名
-     */
-    @Select("select fullName from SongList where uid = #{uid} and listIndex = #{listIndex}")
-    String findSongListFullName (@Param("uid") Integer uid, @Param("listIndex") Integer listIndex);
-    
-    /**
-     * 查找用户歌单中全部歌曲
-     *
-     * @param fullName 歌单在数据库中的表名
-     * @return 歌单中的全部歌曲列表
-     */
-    @Select("select * from `${fullName}`")
-    List<Song> findAllSongInList (String fullName);
-    
-    /**
-     * 查找用户歌单中全部歌曲的 id
-     *
-     * @param fullName 歌单表全名
-     * @return 用户歌单中的歌曲 id
-     */
-    @Select("select url from `${fullName}`")
-    List<String> findAllSongUrl (String fullName);
-    
-    /**
-     * 查找用户的全部动态
-     *
-     * @param uid 用户 uid
-     * @return uid 对应用户的全部动态
-     */
-    @Results(id = "usernameMap", value = {
-            @Result(column = "uid", property = "username", one = @One(select = "com.dao.IUserDao.findUsername", fetchType = FetchType.EAGER))
-    })
-    @Select("select * from #{uid}_news")
-    List<News> findAllNews (Integer uid);
-    
-    /**
-     * 查找用户的全部动态的 id
-     *
-     * @param uid 用户 uid
-     * @return uid 对应用户的全部动态的 id
-     */
-    @Select("select newsId from #{uid}_news")
-    List<Integer> findAllNewsId (Integer uid);
-    
-    /**
-     * 查找用户的歌单后缀
-     *
-     * @param uid 用户 uid
-     * @return 用户当前歌单表名数字后缀
-     */
-    @Select("select listCount from User where uid = #{uid}")
-    Integer findUserListCount (Integer uid);
     
     /**
      * 根据用户 uid 查询用户密码的哈希值
@@ -253,110 +138,6 @@ public interface IUserDao {
      */
     @Update("update User set password = #{newPassword} where uid = #{uid}")
     void updateUserPassword (@Param("uid") Integer uid, @Param("newPassword") Integer newPassword);
-    
-    /**
-     * 添加关注
-     *
-     * @param uid  进行操作的用户 uid
-     * @param fUid 关注的用户的 uid
-     */
-    @Insert("insert into #{uid}_Following select uid, username, sex, birthday, location from User " +
-                    "where uid = #{fUid};" +
-            "insert into #{fUid}_Followers select uid, username, sex, birthday, location from User where uid = #{uid}")
-    void addFollow (@Param("uid") Integer uid, @Param("fUid") Integer fUid);
-    
-    /**
-     * 取消关注
-     *
-     * @param uid  进行操作的用户 uid
-     * @param fUid 被取消关注的用户 uid
-     */
-    @Delete("delete from #{uid}_Following where uid = #{fUid};" +
-            "delete from #{fUid}_Followers where uid = #{uid}")
-    void removeFollow (@Param("uid") Integer uid, @Param("fUid") Integer fUid);
-    
-    /**
-     * 更新用户的粉丝表中的用户的关注表中该用户的信息
-     *
-     * @param fUid 粉丝的 uid
-     * @param user 用户信息
-     */
-    @Update("update #{fUid}_Following " +
-                    "set username = #{user.username}, sex = #{user.sex}, birthday = #{user.birthday}, location = #{user.location}" +
-                    "where uid = #{user.uid}")
-    void updateFollowersListInfo (@Param("fUid") Integer fUid, @Param("user") User user);
-    
-    /**
-     * 创建歌单
-     *
-     * @param uid      用户 uid
-     * @param listId   歌单 id
-     * @param listName 歌单名
-     */
-    @Update("insert into SongList values(#{uid}, #{listId}, #{listName}, '${uid}_Songs_${listId}');" +
-            "update User set listCount = listCount + 1 where uid = #{uid};" +
-            "create table #{uid}_Songs_${listId} (" +
-                    "songIndex int not null," +
-                    "name varchar(30) not null," +
-                    "singer varchar(20)," +
-                    "url varchar(500) not null);")
-    void createSongList (@Param("uid") Integer uid, @Param("listId") Integer listId, @Param("listName") String listName);
-    
-    /**
-     * 删除歌单
-     *
-     * @param fullName 歌单表全名
-     */
-    @Update("delete from SongList where fullName = #{fullName};" +
-            "drop table if exists #{fullName}")
-    void removeSongList (String fullName);
-    
-    /**
-     * 添加歌曲到歌单中
-     *
-     * @param fullName 歌单表的全名
-     * @param song     歌曲
-     */
-    @Insert("insert into `${fullName}` values(#{song.songIndex}, #{song.name}, #{song.singer}, #{song.url})")
-    void addSong (@Param("fullName") String fullName, @Param("song") Song song);
-    
-    /**
-     * 将歌曲从歌单中移除
-     *
-     * @param fullName  歌单表全名
-     * @param songIndex 歌曲索引
-     */
-    @Delete("delete from `${fullName}` where songIndex = #{songIndex}")
-    void removeSong (@Param("fullName") String fullName, @Param("songIndex") Integer songIndex);
-    
-    /**
-     * 更新歌单名
-     *
-     * @param uid       用户 uid
-     * @param listIndex 歌单索引
-     * @param listName  新的歌单名
-     */
-    @Update("update SongList set listName = #{listName} where uid = #{uid} and listIndex = #{listIndex}")
-    void updateSongListName (@Param("uid") Integer uid, @Param("listIndex") Integer listIndex, @Param("listName") String listName);
-    
-    /**
-     * 发布动态
-     *
-     * @param uid  用户 uid
-     * @param info 动态内容
-     * @param time 发布时间
-     */
-    @Insert("insert into #{uid}_news(uid, info, time) values(#{uid}, #{info}, #{time})")
-    void addNews (@Param("uid") Integer uid, @Param("info") String info, @Param("time") Date time);
-    
-    /**
-     * 删除动态
-     *
-     * @param uid    用户 uid
-     * @param newsId 动态 id
-     */
-    @Delete("delete from #{uid}_news where newsId = #{newsId}")
-    void deleteNews (@Param("uid") Integer uid, @Param("newsId") Integer newsId);
     
     /**
      * 删除用户信息以及用户对应的表，用于事务控制
